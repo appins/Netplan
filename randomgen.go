4409772e2f6b2f47de13f1fdd56316dc562f9fe9
Alex Anderson <alexandersonone@gmail.com>
2017-07-17 19:27:31 -0700

    Made some changes to login.html and made the random generator a bit better

--- randomgen.go
+++ randomgen.go
@@ -21,10 +21,10 @@ func randFromArr(arr []string) string {
 
 // Generate a word type thing
 func getRandom() string {
-  vows := []string {"a", "e", "i", "o", "ee"}
+  vows := []string {"a", "e", "i", "o", "ee", "ea", "oo", "y", "ai"}
   cons := []string {"b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p",
-  "r", "s", "t", "v", "w", "x", "ch"}
-  nums := []string {"1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "."}
+  "r", "s", "t", "v", "w", "x", "ch", "ll", "sh", "sh", "th"}
+  nums := []string {"1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-"}
 
   build := ""
 
