415ae274972ba82c9e93501a418897d331a0e5aa
Alex Anderson <alexandersonone@gmail.com>
2017-07-22 20:38:24 -0700

    Added a little note in the comments on the top of the deleteentries.sh file

--- deleteentries.sh
+++ deleteentries.sh
@@ -1,3 +1,6 @@
+# NOTE: This should _NOT_ be used in production!
+# NOTE: This file should be deleted to prevent data loss.
+
 echo "NOTICE: All journal entires will be deleted!"
 echo -n "To continue, please type continue: "
 
