49b1679d1eccc5ae050d86d3821e15daf7b8eb33
Alex Anderson <alexandersonone@gmail.com>
2017-08-26 19:53:25 -0700

    Add a new warning message if the file size of VERSION is different

--- checkversion.sh
+++ checkversion.sh
@@ -2,12 +2,24 @@
 
 curl "https://raw.githubusercontent.com/AppIns/Netplan/master/VERSION" -o CURRENT_VERSION 2> /dev/null
 
+echo
+
 if diff VERSION CURRENT_VERSION > /dev/null; then
-  echo
   echo "Version file matches"
   echo "The repository is up to date"
 else
-  echo
   echo "The server version does not match the repository online!"
   echo "The server is likley outdated."
 fi
+
+VSIZE=$(stat --printf="%s" VERSION)
+CVSIZE=$(stat --printf="%s" CURRENT_VERSION)
+if [[ $VSIZE != $CVSIZE ]]; then
+  echo
+  echo "----------WARNING---------------"
+  echo "The VERSION file online and the local one have different sizes"
+  echo "You might be out of date by a major version"
+  echo "---------------------------------"
+fi
+
+echo
